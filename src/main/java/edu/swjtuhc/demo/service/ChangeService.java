794f993714b0601a2a8fa95f94629e52f6ac6b3e
GGboyzhy <57336618+GGboyzhy@users.noreply.github.com>
2019-12-27 15:04:14 +0800

    Delete ChangeService.java

--- src/main/java/edu/swjtuhc/demo/service/ChangeService.java
+++
@@ -1,8 +0,0 @@
-package edu.swjtuhc.demo.service;
-
-import edu.swjtuhc.demo.model.ChangeModel;
-
-public interface ChangeService {
-	public  int ChangeName(ChangeModel changeName) ;
-	public   void SelectUserByName(ChangeModel changeName);
-}
