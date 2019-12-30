66eb6f13a343f58ccb6e7bc899c70fa0ecdc33f5
18781228822 <493460943@qq.com>
2019-12-30 17:40:56 +0800

    1

---
+++ collection.sql
@@ -0,0 +1,37 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 80017
+ Source Host           : localhost:3306
+ Source Schema         : test
+
+ Target Server Type    : MySQL
+ Target Server Version : 80017
+ File Encoding         : 65001
+
+ Date: 30/12/2019 17:39:44
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for collection
+-- ----------------------------
+DROP TABLE IF EXISTS `collection`;
+CREATE TABLE `collection`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `CollectionName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `author` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `Image` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of collection
+-- ----------------------------
+INSERT INTO `collection` VALUES (1, 'q', 'q', NULL);
+
+SET FOREIGN_KEY_CHECKS = 1;
--- sysuser.sql
+++ sysuser.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80017
  File Encoding         : 65001
 
- Date: 22/12/2019 18:04:26
+ Date: 30/12/2019 17:39:55
 */
 
 SET NAMES utf8mb4;
@@ -22,26 +22,18 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `sysuser`;
 CREATE TABLE `sysuser`  (
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `age` int(11) NULL DEFAULT NULL,
+  `gender` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `emile` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `id` int(11) NOT NULL,
-  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `userpassword` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `userpassword` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of sysuser
 -- ----------------------------
-INSERT INTO `sysuser` VALUES (1, '123', '2');
-INSERT INTO `sysuser` VALUES (2, '13', '12354');
-INSERT INTO `sysuser` VALUES (3, '1823', '12354');
-INSERT INTO `sysuser` VALUES (4, '1923', '12354');
-INSERT INTO `sysuser` VALUES (5, 'user2', 'user12');
-INSERT INTO `sysuser` VALUES (6, 'po2', 'pok');
-INSERT INTO `sysuser` VALUES (7, '123456', '123456');
-INSERT INTO `sysuser` VALUES (8, '5656', '5656');
-INSERT INTO `sysuser` VALUES (9, 'user', 'user');
-INSERT INTO `sysuser` VALUES (10, 'user4', 'user4');
-INSERT INTO `sysuser` VALUES (11, 'po', 'pok');
-INSERT INTO `sysuser` VALUES (12, '1293', '123');
+INSERT INTO `sysuser` VALUES ('张三', 123, '男', '1111', 0, NULL, NULL);
 
 SET FOREIGN_KEY_CHECKS = 1;
